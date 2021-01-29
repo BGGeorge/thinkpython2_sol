91ee4dc0a43bad440dfbe606cb1c6fd1fa125176
BGGeorge <68114669+BGGeorge@users.noreply.github.com>
2021-01-29 23:27:25 +0800

    Delete ex1.py

--- Ch3/ex1.py
+++
@@ -1,7 +0,0 @@
-# Exercise 3.1. Write a function named right_justify that takes a string named s as a parameter and prints the string with enough leading spaces so that the last letter of the string is in column 70
-# of the display.
-
-def right_justify(s):
-    nspace = 70 - len(s)
-    print(nspace * ' ' + s)
-right_justify('monty')
