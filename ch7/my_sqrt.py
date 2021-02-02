7398c2e4a6b036f6edacab2483879f01252d4350
BGGeorge <68114669+BGGeorge@users.noreply.github.com>
2021-02-02 23:28:34 +0800

    Add files via upload

---
+++ ch7/my_sqrt.py
@@ -0,0 +1,25 @@
+# import math
+'''
+def eval_loop():
+    while True:
+        user_input = input("Please type: ")
+        
+        if eval(user_input) == done:  
+            break
+        print(eval(user_input))
+    return eval(user_input)
+
+eval_loop()
+'''
+
+# import math
+
+def eval_loop():
+    while True:
+        user_input = input("Please type: ")
+        
+        if eval(user_input) == 'done':  
+            break
+        print(user_input)
+
+eval_loop()
